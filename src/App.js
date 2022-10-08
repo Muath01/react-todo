b3926162f7e3fbf70b63dd79b59453c08f619b8c
Muath <muath.khalifa@yahoo.com>
2022-10-08 08:59:27 +0100

    Form and todo components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import react, {useState} from "react";
 import './App.css';
+import Form from "./components/Form"
+import TodoList from './components/TodoList';
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <header>
+        <h1>To-Do List{inputText}</h1>
+      </header>
+
+      <Form setInputText={setInputText}/> 
+      <TodoList />
     </div>
   );
 }
