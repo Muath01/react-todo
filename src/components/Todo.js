471168b82e65251b1641d219cfcbb6deae50e524
Muath <muath.khalifa@yahoo.com>
2022-10-08 10:37:25 +0100

    trash button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,30 @@
 import React from "react";
 
-const Todo = () =>{
+const Todo = ({todo, todos, setTodos}) =>{
+    console.log(todo.text)
+
+    function completedHandler(e){
+        setTodos(todos.map(item => {
+            if(item.id === todo.id){
+                return{
+                    ...item,
+                    completed: !item.completed
+                }
+                return item;
+            }
+        }))
+    }
+
+    const deleteHandler = () =>{
+         setTodos(todos.filter(el => el.id !== todo.id))
+    }
     return(
         <div className="todo">
-            <li className="todo-item">
-                Heyyy
+            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
+                {todo.text}
             </li>
-            <button className="complete-btn" ><i className="fas fa-check"></i></button>
-            <button className="trash-btn"><i className="fas fa-trash"></i></button>
+            <button onClick={completedHandler} className="complete-btn" ><i className="fas fa-check"></i></button>
+            <button onClick={deleteHandler} className="trash-btn"><i className="fas fa-trash"></i></button>
         </div>
     )
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,13 +5,18 @@ import react from "react"
 
 import Todo from "./Todo";
 
-const TodoList = ({todos}) => {
+const TodoList = ({todos, setTodos}) => {
     
-    console.log(todos)
     return(
         <div className="todo-container">
             <ul className="todo-list">
-               
+               {
+                todos.map(todo => (
+                    <Todo todos={todos} setTodos={setTodos} key={todo.id} todo={todo}/>
+                    
+                    )
+                )
+               }
             </ul>
         </div>
     )
